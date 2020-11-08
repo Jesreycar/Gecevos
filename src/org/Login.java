c6ac9881410b97ce05f75b6e8cc99bd5dc3c714f
Jesus Reyes Carrillo <dam1ajesusreyescarrillo@iesantoniogala.es>
2020-11-08 10:40:49 +0100

    Version 1.0 Comienzo aplicación Login, Registro

---
+++ src/org/Login.java
@@ -0,0 +1,196 @@
+package org;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.AbstractBorder;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.JFormattedTextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.font.TextAttribute;
+import java.util.Map;
+import java.awt.SystemColor;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPasswordField;
+import java.awt.Rectangle;
+
+public class Login extends JFrame {
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	private static boolean mostrar = false;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login login = new Login();
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Login() {
+		setVisible(true);
+		setTitle("Gecevos");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(45,118,116));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(Color.BLACK);
+		panel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		panel.setBounds(70, 55, 450, 350);
+		panel.setBackground(new Color(118,45,47));
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel = new JLabel("Usuario:");
+		lblNewLabel.setFont(new Font("Dialog", Font.PLAIN, 15));
+		lblNewLabel.setForeground(Color.WHITE);
+		
+		textField = new JTextField();
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setForeground(Color.WHITE);
+		lblContrasea.setFont(new Font("Dialog", Font.PLAIN, 15));
+		
+		JButton btnAcceder = new JButton("Acceder");
+		btnAcceder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {			
+			}
+		});
+		
+		JTextPane txtpntodavaNoTienes = new JTextPane();
+		txtpntodavaNoTienes.setBackground(new Color(118,45,47));
+		txtpntodavaNoTienes.setEditable(false);
+		txtpntodavaNoTienes.setFont(new Font("Dialog", Font.PLAIN, 14));
+		txtpntodavaNoTienes.setForeground(Color.WHITE);
+		txtpntodavaNoTienes.setText("\u00BFTodav\u00EDa no tienes cuenta?");
+		
+		JLabel lblRegistreseAqu = new JLabel();
+		lblRegistreseAqu.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		//"Registrese Aqu\u00ED
+		lblRegistreseAqu.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				setVisible(false);
+				Registro registro = new Registro();
+			}
+		});
+		lblRegistreseAqu.setForeground(new Color(12,124,186));
+		lblRegistreseAqu.setText("<html>Registrese <u>Aqui</u></html>");
+		
+		passwordField = new JPasswordField();
+		passwordField.setEchoChar('*');
+		
+		JLabel lblImg = new JLabel("");
+		lblImg.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(mostrar) {
+					Icon icono = new ImageIcon(new ImageIcon("img/Mostrar.png").getImage().getScaledInstance(lblImg.getWidth(), lblImg.getHeight(), Image.SCALE_AREA_AVERAGING));
+					lblImg.setIcon(icono);
+					passwordField.setEchoChar((char)0);
+					mostrar = false;
+				}else {
+					Icon icono = new ImageIcon(new ImageIcon("img/Ocultar.png").getImage().getScaledInstance(lblImg.getWidth(), lblImg.getHeight(), Image.SCALE_AREA_AVERAGING));
+					lblImg.setIcon(icono);
+					passwordField.setEchoChar('*');
+					mostrar = true;
+				}
+			}
+		});
+		lblImg.setBounds(new Rectangle(0, 0, 20, 20));
+		Icon icono = new ImageIcon(new ImageIcon("img/Mostrar.png").getImage().getScaledInstance(lblImg.getWidth(), lblImg.getHeight(), Image.SCALE_AREA_AVERAGING));
+		lblImg.setIcon(icono);
+		lblImg.setForeground(Color.WHITE);
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblImg, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+							.addGroup(gl_panel.createSequentialGroup()
+								.addGap(129)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+									.addGroup(gl_panel.createSequentialGroup()
+										.addGap(40)
+										.addComponent(lblRegistreseAqu, GroupLayout.PREFERRED_SIZE, 103, GroupLayout.PREFERRED_SIZE))
+									.addComponent(txtpntodavaNoTienes, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+							.addGroup(gl_panel.createSequentialGroup()
+								.addGap(162)
+								.addComponent(btnAcceder, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))
+							.addGroup(gl_panel.createSequentialGroup()
+								.addGap(41)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+									.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+									.addComponent(textField, GroupLayout.PREFERRED_SIZE, 364, GroupLayout.PREFERRED_SIZE)))
+							.addGroup(gl_panel.createSequentialGroup()
+								.addGap(41)
+								.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+									.addComponent(passwordField, GroupLayout.DEFAULT_SIZE, 364, Short.MAX_VALUE)
+									.addComponent(lblContrasea, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE)))))
+					.addGap(138))
+		);
+		
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(54)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addGap(36)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblContrasea, GroupLayout.PREFERRED_SIZE, 14, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblImg, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addGap(38)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup()
+							.addGap(20)
+							.addComponent(lblRegistreseAqu, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(txtpntodavaNoTienes, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
+					.addGap(8)
+					.addComponent(btnAcceder, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addGap(32))
+		);
+		panel.setLayout(gl_panel);
+	}
+}
---
+++ src/org/Registro.java
@@ -0,0 +1,134 @@
+package org;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+
+public class Registro extends JFrame {
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+
+	/**
+	 * Create the frame.
+	 */
+	public Registro() {
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new java.awt.Color(45,118,116));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		panel.setBackground(new Color(118, 45, 47));
+		panel.setBounds(45, 27, 500, 400);
+		contentPane.add(panel);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setForeground(Color.WHITE);
+		lblNombre.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		lblNombre.setBounds(10, 50, 70, 14);
+		panel.add(lblNombre);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(10, 75, 212, 30);
+		panel.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(10, 214, 212, 30);
+		panel.add(textField_1);
+		
+		JButton btnRegistrarse = new JButton("Registrarse");
+		btnRegistrarse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnRegistrarse.setBounds(101, 328, 121, 30);
+		panel.add(btnRegistrarse);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(10, 144, 212, 30);
+		panel.add(textField_2);
+		
+		JLabel lblApellidos = new JLabel("Apellidos:");
+		lblApellidos.setForeground(Color.WHITE);
+		lblApellidos.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		lblApellidos.setBounds(10, 119, 70, 14);
+		panel.add(lblApellidos);
+		
+		JLabel lblDni = new JLabel("DNI:");
+		lblDni.setForeground(Color.WHITE);
+		lblDni.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		lblDni.setBounds(10, 189, 70, 14);
+		panel.add(lblDni);
+		
+		JButton btnVolverAlLogin = new JButton("Volver al login");
+		btnVolverAlLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				Login frame = new Login();
+			}
+		});
+		btnVolverAlLogin.setBounds(280, 328, 121, 30);
+		panel.add(btnVolverAlLogin);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(278, 119, 212, 30);
+		panel.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(278, 193, 212, 30);
+		panel.add(textField_4);
+		
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setForeground(Color.WHITE);
+		lblContrasea.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		lblContrasea.setBounds(280, 91, 93, 14);
+		panel.add(lblContrasea);
+		
+		JLabel lblConfirmaLaContrasea = new JLabel("Confirma la contrase\u00F1a");
+		lblConfirmaLaContrasea.setForeground(Color.WHITE);
+		lblConfirmaLaContrasea.setFont(new Font("Comic Sans MS", Font.PLAIN, 14));
+		lblConfirmaLaContrasea.setBounds(280, 168, 170, 14);
+		panel.add(lblConfirmaLaContrasea);
+		
+		JRadioButton rdbtnAlumno = new JRadioButton("Alumno");
+		rdbtnAlumno.setHorizontalAlignment(SwingConstants.RIGHT);
+		rdbtnAlumno.setForeground(Color.WHITE);
+		rdbtnAlumno.setBackground(new Color(118, 45, 47));
+		rdbtnAlumno.setBounds(146, 22, 109, 23);
+		panel.add(rdbtnAlumno);
+		
+		JRadioButton rdbtnProfesor = new JRadioButton("Profesor");
+		rdbtnProfesor.setForeground(Color.WHITE);
+		rdbtnProfesor.setBackground(new Color(118, 45, 47));
+		rdbtnProfesor.setBounds(257, 22, 109, 23);
+		panel.add(rdbtnProfesor);
+	}
+}
